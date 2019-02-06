ff1a9de82b85213632d2b668f80e0f5db724574e
Naveed <nsaleem@edx.org>
2019-02-06 18:37:28 +0500

    b

--- resources/setup_emulator.sh
+++ resources/setup_emulator.sh
@@ -11,6 +11,7 @@ kill_all_emus() {
 
 echo 'Creating emulator'
 # chmod 777 -R /root/.android
+chmod 777 $USER:$USER /opt/android-sdk-linux/tools/bin/avdmanager
 echo 'n' | $ANDROID_HOME/tools/bin/avdmanager create avd -n test-android -f -k "system-images;android-27;google_apis;x86"
 
 echo 'loading emulator'
