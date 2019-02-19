066198c2f1103d9a5a52c173d602e76496639aa2
Naveed <nsaleem@edx.org>
2019-02-19 19:24:38 +0500

    a

--- resources/setup_emulator.sh
+++ resources/setup_emulator.sh
@@ -10,18 +10,16 @@ kill_all_emus() {
 }
 
 echo 'Creating emulator'
-# chmod 777 -R /root/.android
-# chmod 777 $ANDROID_HOME/tools/bin/avdmanager
-# $ANDROID_HOME/tools/bin/avdmanager
 
-echo 'n' | $ANDROID_HOME/tools/bin/avdmanager create avd -n test-android -f -k "system-images;android-27;google_apis;x86"
+echo 'n' | $ANDROID_HOME/tools/bin/avdmanager create avd -n test-android -f -k "system-images;android-25;google_apis;armeabi-v7a"
+
+# "system-images;android-27;google_apis;x86"
 
 echo 'loading emulator'
 $ANDROID_HOME/emulator/emulator -avd test-android -no-boot-anim 
-
 # -no-window -noaudio -verbose & 
 
-sleep 30
+sleep 45
 
 # Wait until the newly created emulator has finished booting before running tests
 while true; do
