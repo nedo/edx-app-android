5d71e67aa4de076aa38ba9f2d99481ea8df9eb58
Naveed <nsaleem@edx.org>
2019-02-28 16:54:09 +0500

    1

--- resources/install_prereqs.sh
+++ resources/install_prereqs.sh
@@ -2,10 +2,21 @@
 
 set -e
 
+PYTHON_ENVIRONMENT_NAME="python_env"
+NODE_ENVIRONMENT_NAME="node_env"
+
 # Install python requirements 
-ENVIRONMENT_NAME="mobile_automation_env"
-
-virtualenv "$ENVIRONMENT_NAME"
-source "./$ENVIRONMENT_NAME/bin/activate"
-
+virtualenv "$PYTHON_ENVIRONMENT_NAME"
+source "./$PYTHON_ENVIRONMENT_NAME/bin/activate"
 pip install -r ./edx-app-test/requirements.txt
+pip install nodeenv
+
+# Install node requirements 
+nodeenv "$NODE_ENVIRONMENT_NAME"
+source "./$NODE_ENVIRONMENT_NAME/bin/activate"
+
+npm install appium 
+
+appium & 
+
+pytest 
