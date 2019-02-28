e4dab35fa66ad8281d120faa16e2730e5af811da
Naveed <nsaleem@edx.org>
2019-02-28 17:59:35 +0500

    1

--- resources/install_prereqs.sh
+++ resources/install_prereqs.sh
@@ -5,13 +5,13 @@ set -e
 PYTHON_ENVIRONMENT_NAME="python_env"
 NODE_ENVIRONMENT_NAME="node_env"
 
-# setup python virutal env and install python tools 
+# setup python virutal env and install dependencies
 virtualenv "$PYTHON_ENVIRONMENT_NAME"
 source "./$PYTHON_ENVIRONMENT_NAME/bin/activate"
 pip install -r ./edx-app-test/requirements.txt
 pip install nodeenv
 
-# setup node virutal env and install node tool
+# setup node virutal env and install dependencies
 nodeenv "$NODE_ENVIRONMENT_NAME"
 source "./$NODE_ENVIRONMENT_NAME/bin/activate"
 npm install appium 
